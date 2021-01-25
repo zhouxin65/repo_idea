79920ed29b245b40ddc7643d5aea4c1763d86f90
ZhouXin <70927585+zhouxin65@users.noreply.github.com>
2021-01-25 20:19:06 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -52,13 +52,13 @@ public interface UserMapper {
     List<Resource> findResourceByRoleId2(List<Integer> ids);
 
 
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
+    public void test11();
+    public void test12();
+    public void test13();
+    public void test14();
+    public void test15();
+    public void test16();
+    public void test17();
 
 
 }
